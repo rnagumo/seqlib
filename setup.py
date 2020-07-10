e5efc547c940cec78d6edf3e2dc16f23cec22b94
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-07-10 18:26:02 +0900

    Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "torch==1.5.1",
+]
+
+
+setup(
+    name="seqlib",
+    version="0.1",
+    description="2D sequential models by PyTorch",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
