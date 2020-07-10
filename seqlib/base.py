2bd07f51620d48845276ee48ed97af946cfc2429
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-07-11 08:08:12 +0900

    Add loss method

--- seqlib/base.py
+++ seqlib/base.py
@@ -3,6 +3,8 @@
 
 from typing import Optional, Tuple, Dict
 
+import math
+
 from torch import Tensor, nn
 
 
@@ -52,3 +54,54 @@ class BaseSequentialVAE(nn.Module):
         """
 
         raise NotImplementedError
+
+
+def kl_divergence_normal(mu0: Tensor, var0: Tensor, mu1: Tensor, var1: Tensor,
+                         reduce: bool = True) -> Tensor:
+    """Kullback Leibler divergence for 1-D Normal distributions.
+
+    p = N(mu0, var0)
+    q = N(mu1, var1)
+    KL(p||q) = 1/2 * (var0/var1 + (mu1-mu0)^2/var1 - 1 + log(var1/var0))
+
+    Args:
+        mu0 (torch.Tensor): Mean vector of p, size.
+        var0 (torch.Tensor): Diagonal variance of p.
+        mu1 (torch.Tensor): Mean vector of q.
+        var1 (torch.Tensor): Diagonal variance of q.
+        reduce (bool, optional): If `True`, sum calculated loss for each
+            data point.
+
+    Returns:
+        kl (torch.Tensor): Calculated kl divergence for each data.
+    """
+
+    diff = mu1 - mu0
+    kl = (var0 / var1 + diff ** 2 / var1 - 1 + (var1 / var0).log()) * 0.5
+
+    if reduce:
+        return kl.sum(-1)
+    return kl
+
+
+def nll_normal(x: Tensor, mu: Tensor, var: Tensor, reduce: bool = True
+               ) -> Tensor:
+    """Negative log likelihood for 1-D Normal distribution.
+
+    Args:
+        x (torch.Tensor): Inputs tensor, size `(*, dim)`.
+        mu (torch.Tensor): Mean vector, size `(*, dim)`.
+        var (torch.Tensor): Variance vector, size `(*, dim)`.
+        reduce (bool, optional): If `True`, sum calculated loss for each
+            data point.
+
+    Returns:
+        nll (torch.Tensor): Calculated nll for each data, size `(*,)` if
+            `reduce` is `True`, `(*, dim)` otherwise.
+    """
+
+    nll = 0.5 * ((2 * math.pi * var).log() + (x - mu) ** 2 / var)
+
+    if reduce:
+        return nll.sum(-1)
+    return nll
