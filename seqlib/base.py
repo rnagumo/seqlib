958cf0051e56d9e1828618c27f8081b29abaf1c7
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-07-11 07:40:12 +0900

    Update base class method

--- seqlib/base.py
+++ seqlib/base.py
@@ -23,45 +23,48 @@ class BaseSequentialModel(nn.Module):
 
         return recon
 
-    def loss_func(self, x: Tensor) -> Dict[str, Tensor]:
+    def loss_func(self, x: Tensor, beta: float = 1.0) -> Dict[str, Tensor]:
         """Loss function.
 
         Args:
             x (torch.Tensor): Observation tensor, size `(b, l, c, h, w)`.
+            beta (float, optional): Beta coefficient of KL term.
 
         Returns:
             loss_dict (dict of [str, torch.Tensor]): Dict of lossses.
         """
 
-        _, loss_dict = self.inference(x)
+        _, loss_dict = self.inference(x, beta)
 
         return loss_dict
 
-    def inference(self, x: Tensor
+    def inference(self, x: Tensor, beta: float = 1.0
                   ) -> Tuple[Tuple[Tensor, ...], Dict[str, Tensor]]:
         """Inferences with given observations.
 
         Args:
             x (torch.Tensor): Observation tensor, size `(b, l, c, h, w)`.
+            beta (float, optional): Beta coefficient of KL term.
 
         Returns:
             data (tuple of torch.Tensor): Tuple of reconstructed observations
-                and latent variables. The first element of the tuple will be
-                returned in `forward` method.
+                and latent variables. The first element of the tuple should be
+                recontstructed observations.
             loss_dict (dict of [str, torch.Tensor]): Dict of lossses.
         """
 
         raise NotImplementedError
 
-    def sample(self, z: Optional[Tensor] = None) -> Tensor:
-        """Samples observations.
+    def sample(self, x: Optional[Tensor] = None, time_step: int = 0) -> Tensor:
+        """Reconstructs and samples observations.
 
         Args:
-            z (torch.Tensor, optional): Latent variable, size `(b, l, d)`.
+            x (torch.Tensor): Observation tensor, size `(b, l, c, h, w)`.
             time_step (int, optional): Time step for prediction.
 
         Returns:
-            x (torch.Tensor): Sampled observations, size `(b, l, c, h, w)`.
+            sample (torch.Tensor): Reconstructed and sampled observations, size
+                `(b, l + t, c, h, w)`.
         """
 
         raise NotImplementedError
