d37190a6d9c02248932d5d9688667be8ed7e990a
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-08-31 12:09:05 +0900

    Update test cases using pytest

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,64 +1,57 @@
 
-import unittest
-
 import torch
-
 import seqlib
 
 
-class TestUtilMethods(unittest.TestCase):
+def test_kl_batch() -> None:
+    batch = 10
+    x_dim = 5
 
-    def test_kl_batch(self):
-        batch = 10
-        x_dim = 5
+    mu0 = torch.randn(batch, x_dim)
+    var0 = torch.rand(batch, x_dim) + 0.01
+    mu1 = torch.randn(batch, x_dim)
+    var1 = torch.rand(batch, x_dim) + 0.01
 
-        mu0 = torch.randn(batch, x_dim)
-        var0 = torch.rand(batch, x_dim) + 0.01
-        mu1 = torch.randn(batch, x_dim)
-        var1 = torch.rand(batch, x_dim) + 0.01
-
-        kl = seqlib.kl_divergence_normal(mu0, var0, mu1, var1)
-        self.assertTupleEqual(kl.size(), (batch,))
-        self.assertTrue((kl >= 0).all())
-
-    def test_kl_batch_num(self):
-        batch = 10
-        num_points = 8
-        x_dim = 5
-
-        mu0 = torch.randn(batch, num_points, x_dim)
-        var0 = torch.rand(batch, num_points, x_dim) + 0.01
-        mu1 = torch.randn(batch, num_points, x_dim)
-        var1 = torch.rand(batch, num_points, x_dim) + 0.01
-
-        kl = seqlib.kl_divergence_normal(mu0, var0, mu1, var1)
-        self.assertTupleEqual(kl.size(), (batch, num_points))
-        self.assertTrue((kl >= 0).all())
-
-    def test_kl_same(self):
-        batch = 10
-        x_dim = 5
-
-        mu0 = torch.randn(batch, x_dim)
-        var0 = torch.rand(batch, x_dim) + 0.01
-
-        kl = seqlib.kl_divergence_normal(mu0, var0, mu0, var0)
-        self.assertTupleEqual(kl.size(), (batch,))
-        self.assertTrue((kl == 0).all())
-
-    def test_nll_bernoulli(self):
-        batch = 10
-        x_dim = 5
-
-        x = torch.rand(batch, x_dim)
-        probs = torch.rand(batch, x_dim)
-
-        nll = seqlib.nll_bernoulli(x, probs)
-
-        print(nll)
-        self.assertTupleEqual(nll.size(), (batch,))
-        self.assertTrue((nll >= 0).all())
+    kl = seqlib.kl_divergence_normal(mu0, var0, mu1, var1)
+    assert kl.size() == (batch,)
+    assert (kl >= 0).all()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_kl_batch_num() -> None:
+    batch = 10
+    num_points = 8
+    x_dim = 5
+
+    mu0 = torch.randn(batch, num_points, x_dim)
+    var0 = torch.rand(batch, num_points, x_dim) + 0.01
+    mu1 = torch.randn(batch, num_points, x_dim)
+    var1 = torch.rand(batch, num_points, x_dim) + 0.01
+
+    kl = seqlib.kl_divergence_normal(mu0, var0, mu1, var1)
+    assert kl.size() == (batch, num_points)
+    assert (kl >= 0).all()
+
+
+def test_kl_same() -> None:
+    batch = 10
+    x_dim = 5
+
+    mu0 = torch.randn(batch, x_dim)
+    var0 = torch.rand(batch, x_dim) + 0.01
+
+    kl = seqlib.kl_divergence_normal(mu0, var0, mu0, var0)
+    assert kl.size() == (batch,)
+    assert (kl >= 0).all()
+
+
+def test_nll_bernoulli() -> None:
+    batch = 10
+    x_dim = 5
+
+    x = torch.rand(batch, x_dim)
+    probs = torch.rand(batch, x_dim)
+
+    nll = seqlib.nll_bernoulli(x, probs)
+
+    assert nll.size() == (batch,)
+    assert (nll >= 0).all()
